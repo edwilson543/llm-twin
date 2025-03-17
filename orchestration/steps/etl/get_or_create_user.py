c8b02561213b34a15456ec1bc1196fd76331ebf2
edwilson <edward.e.j.wilson@gmail.com>
2025-03-17 07:09:52 +0000

    Add a step for getting or creating user

---
+++ orchestration/steps/etl/get_or_create_user.py
@@ -0,0 +1,44 @@
+import typing
+
+import loguru
+import zenml
+
+from llm_twin.application import utils
+from llm_twin.data import documents
+
+
+@zenml.step
+def get_or_create_user(
+    user_full_name: str,
+) -> typing.Annotated[documents.UserDocument, "user"]:
+    loguru.logger.info(f"Getting or creating user: {user_full_name}")
+
+    name = utils.split_user_full_name(user_full_name)
+    user_document = documents.UserDocument.get_or_create(
+        first_name=name.first_name, last_name=name.last_name
+    )
+
+    step_context = zenml.get_step_context()
+    step_context.add_output_metadata(
+        output_name="user",
+        metadata=_get_metadata(
+            user_full_name=user_full_name, user_document=user_document
+        ),
+    )
+
+    return user_document
+
+
+def _get_metadata(
+    *, user_full_name: str, user_document: documents.UserDocument
+) -> dict[str, typing.Any]:
+    return {
+        "query": {
+            "user_full_name": user_full_name,
+        },
+        "retrieved": {
+            "user_id": str(user_document.id),
+            "first_name": str(user_document.first_name),
+            "last_name": str(user_document.last_name),
+        },
+    }
