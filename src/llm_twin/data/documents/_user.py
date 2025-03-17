26331780a29fd97ec7b69295aa06974dc994f194
edwilson <edward.e.j.wilson@gmail.com>
2025-03-17 06:57:00 +0000

    Add user document class

---
+++ src/llm_twin/data/documents/_user.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from . import _base
+
+
+class UserDocument(_base.NoSQLBaseDocument):
+    first_name: str
+    last_name: str
+
+    @classmethod
+    def _get_collection_name(cls) -> str:
+        return "users"
+
+    @property
+    def full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
