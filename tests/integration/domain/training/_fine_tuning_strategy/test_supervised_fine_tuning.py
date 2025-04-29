8eff0727e2ebdc36b844ee30f6afdd344abf0535
edwilson <edward.e.j.wilson@gmail.com>
2025-04-29 08:43:21 +0100

    Re-use fixture for creating model output dir

--- tests/integration/domain/training/_fine_tuning_strategy/test_supervised_fine_tuning.py
+++ tests/integration/domain/training/_fine_tuning_strategy/test_supervised_fine_tuning.py
@@ -1,9 +1,3 @@
-import pathlib
-import shutil
-import typing
-import uuid
-
-import pytest
 import torch
 import transformers
 
@@ -12,16 +6,6 @@ from testing.helpers import training as training_helpers
 
 
 class TestFineTune:
-    @pytest.fixture
-    def output_dir(self) -> typing.Generator[pathlib.Path, None, None]:
-        output_dir = pathlib.Path(__file__).parent / "outputs" / str(uuid.uuid4())
-        output_dir.mkdir(parents=True, exist_ok=False)
-
-        try:
-            yield output_dir
-        finally:
-            shutil.rmtree(output_dir)
-
     def test_can_fine_tune_tiny_random_llama_on_fake_dataset(self, output_dir):
         model_name = "llamafactory/tiny-random-Llama-3"
         data_loader = training_helpers.FakeDatasetLoader()
@@ -31,7 +15,7 @@ class TestFineTune:
             model_name=model_name,
             output_dir=output_dir,
             num_train_epochs=1,
-            report_to=None, # Don't report training data anywhere.
+            report_to=None,  # Don't report training data anywhere.
             optimizer="adamw_torch",  # `adamw_8bit` not available on MacOS.
         )
 
