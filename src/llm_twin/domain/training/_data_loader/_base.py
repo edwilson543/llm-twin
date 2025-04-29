e0d11b78d80473002a9fbaef3c6acf8a6c516f18
edwilson <edward.e.j.wilson@gmail.com>
2025-04-29 07:58:58 +0100

    Update return type of data loader base class

--- src/llm_twin/domain/training/_data_loader/_base.py
+++ src/llm_twin/domain/training/_data_loader/_base.py
@@ -1,9 +1,11 @@
 import abc
 import dataclasses
 
+from llm_twin.domain import dataset_generation
+
 
 @dataclasses.dataclass(frozen=True)
 class DataLoader(abc.ABC):
     @abc.abstractmethod
-    def load(self) -> dict:
+    def load(self) -> dataset_generation.TrainTestSplit:
         raise NotImplementedError
--- src/llm_twin/domain/training/_data_loader/_hugging_face.py
+++ src/llm_twin/domain/training/_data_loader/_hugging_face.py
@@ -2,6 +2,8 @@ import dataclasses
 
 import datasets
 
+from llm_twin.domain import dataset_generation
+
 from . import _base
 
 
@@ -10,28 +12,6 @@ class HuggingFaceDataLoader(_base.DataLoader):
     dataset_path: str
     eos_token: str
 
-    def load(self) -> datasets.DatasetDict:
+    def load(self) -> dataset_generation.TrainTestSplit:
         dataset = datasets.load_dataset(self.dataset_path, split="train")
-
-        dataset = dataset.map(
-            self._format_samples, batched=True, remove_columns=dataset.column_names
-        )
-        return dataset.train_test_split(test_size=0.05)
-
-    def _format_samples(self, samples: list[dict]) -> dict[str, list[str]]:
-        return {"text": [self._format_sample(sample) for sample in samples]}
-
-    def _format_sample(self, sample: dict) -> str:
-        return (
-            ALPACA_TEMPLATE.format(sample["instruction"], sample["answer"])
-            + self.dataset_path
-        )
-
-
-ALPACA_TEMPLATE = """Below is an instruction that describes a task. Write a response that appropriately completes the request.
-
-### Instruction:
-{}
-
-### Response:
-{}"""
+        return dataset_generation.TrainTestSplit.model_validate(dataset)
